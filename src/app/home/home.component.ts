f4999d1db6135426c01b624aed1ce46bfe564115
matheushahah <78930361+Matheus-fast@users.noreply.github.com>
2025-06-09 22:27:24 -0300

    feat: adicionando detalhes do produto

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -24,7 +24,7 @@ export class HomeComponent {
       nome:"Oreo sabor morango",
       valor:3.99,
       descricao: "Biscoito recheado de morango 90g",
-      keywords: ["biscoito", "morango", "oreo"],
+      keywords: ["BISCOITO", "OREO", "MORANGO"],
       categoria: "Oreo",
       quantidade: 0
     },
@@ -33,7 +33,7 @@ export class HomeComponent {
       nome:"Oreo sabor chocolate",
       valor:3.99,
       descricao: "Biscoito recheado de chocolate 90g",
-      keywords: ["biscoito", "chocolate", "oreo"],
+      keywords: ["BISCOITO", "OREO", "CHCOLATE"],
       categoria: "Oreo",
       quantidade: 0
     },
@@ -43,9 +43,33 @@ export class HomeComponent {
       nome: "Oreo sabor limão",
       valor: 3.99,
       descricao: "Biscoito recheado de limão 90g",
-      keywords: ["biscoito", "limão", "oreo"],
+      keywords: ["BISCOITO",  "OREO", "LIMÃO"],
       categoria: "Oreo",
       quantidade: 0
     }
   ];
+<<<<<<< Updated upstream
+=======
+
+  adicionarBolacha(bolacha: Bolacha) {
+    let json = localStorage.getItem("bolachas");
+    let cesta = new Cesta();
+
+    if (json != null) {
+      cesta = JSON.parse(json);
+    } 
+
+    cesta.itens.push(bolacha);
+    cesta.total += bolacha.valor;
+    
+    localStorage.setItem("bolachas", JSON.stringify(cesta));
+    location.href = "cesta";
+  }
+
+  verDetalhes(bolacha: Bolacha) {
+    localStorage.setItem("bolacha", JSON.stringify(bolacha));
+    location.href = "detalhes";
+  }
+
+>>>>>>> Stashed changes
 }
