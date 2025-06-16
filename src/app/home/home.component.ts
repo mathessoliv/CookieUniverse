1799bfef8af8d6328ade6f9fb5b3cac76c908312
matheushahah <78930361+Matheus-fast@users.noreply.github.com>
2025-06-15 23:10:45 -0300

    #004a feat: barra de pesquisa

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -51,6 +51,24 @@ export class HomeComponent {
     }
   ];
 
+  public listaFiltrada: Bolacha[] = [];
+
+  constructor() {
+    let json = localStorage.getItem("filtro");
+    if (json != null) {
+        let filtro = json;
+
+        if (filtro != "") this.fazerBusca(filtro);
+    }
+  }
+
+  fazerBusca(filtro: string) {
+    this.mensagem = "Pesquisa por: " + filtro;
+    this.listaFiltrada = this.bolachas;
+    this.bolachas = this.listaFiltrada.filter(Bolacha => Bolacha.nome.toLowerCase().includes(filtro.toLowerCase())
+      );
+    }
+
   adicionarBolacha(bolacha: Bolacha) {
     let json = localStorage.getItem("cesta");
     let cesta = new Cesta();
